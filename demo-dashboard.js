b300910733f3384e66a8d5e3bad0373cf81f02d9
Manimozhi Neethimohan <Manimozhin@gain-insights.com>
2023-06-26 18:59:29 +0530

    changes

--- demo-dashboard.js
+++ demo-dashboard.js
@@ -24,4 +24,182 @@ require.config({
     "resources",
 });
 
-require(["js/qlik"], function (qlik) {});
+require(["js/qlik"], function (qlik) {
+  var app = qlik.openApp("a92e83cb-98b5-4c02-9dad-753067b309bd", config);
+  var app1 = qlik.openApp("509332f9-1461-4a37-8b3f-84eab2666ec4", config);
+  app1.getObject("ob", "fxyuxvp");
+  // app1.getObject("kpi1", "JRNGq");
+  // app1.getObject("kpi2", "PUpAQty");
+  // app1.getObject("kpi3", "sKDevh");
+  app1.getObject("kpi4", "sKDevh");
+  app1.getObject("pie", "cXahBQ");
+  // app.getObject("table", "pvJDPB");
+  app.getObject("chartobj", "tTZQUX");
+  //callbacks -- inserted here --
+  function CustomTable(reply, app) {
+    $.each(reply.qHyperCube.qDataPages[0].qMatrix, function (k, val) {
+      if (k <4) {
+        var html =
+          "<tr><td>" +
+          val[0].qText +
+          "</td>" +
+          "<td>" +
+          val[1].qText +
+          "</td></tr>";
+       
+        $("#customTable").append(html);
+      }
+      // $("td").css({
+      //   height: "10%",
+      //   "background-color": "var(--grey)",
+      //   width: "30%",
+      //   padding: "10px",
+      // });
+     
+    });
+  }
+
+  //create cubes and lists -- inserted here --
+  app.createCube(
+    {
+      qInitialDataFetch: [
+        {
+          qHeight: 20,
+          qWidth: 2,
+        },
+      ],
+      qDimensions: [
+        {
+          qDef: {
+            qFieldDefs: ["Product Category"],
+          },
+          qNullSuppression: true,
+          qOtherTotalSpec: {
+            qOtherMode: "OTHER_OFF",
+            qSuppressOther: true,
+            qOtherSortMode: "OTHER_SORT_DESCENDING",
+            qOtherCounted: {
+              qv: "5",
+            },
+            qOtherLimitMode: "OTHER_GE_LIMIT",
+          },
+        },
+      ],
+      qMeasures: [
+        {
+          qDef: {
+            qDef: "Count(Product)",
+          },
+          qLabel: "Product",
+          qLibraryId: null,
+          qSortBy: {
+            qSortByState: 0,
+            qSortByFrequency: 0,
+            qSortByNumeric: 0,
+            qSortByAscii: 1,
+            qSortByLoadOrder: 0,
+            qSortByExpression: 0,
+            qExpression: {
+              qv: " ",
+            },
+          },
+        },
+      ],
+      qSuppressZero: false,
+      qSuppressMissing: false,
+      qMode: "S",
+      qInterColumnSortOrder: [],
+      qStateName: "$",
+    },
+    CustomTable
+  );
+
+  $(document).ready(function () {
+    //Main-content Switch
+    function removeActiveMain() {
+      $("#list li").removeClass("activelist");
+      $("#content .main").removeClass("activemain");
+    }
+
+    $("#list li").each(function (i) {
+      $(this).on("click", function () {
+        removeActiveMain();
+        $(this).addClass("activelist");
+        $("#content .main").eq(i).addClass("activemain");
+        //qlik resize
+        qlik.resize();
+      });
+    });
+    //Tab Switch
+    function removeActive() {
+      $("#tabs span").removeClass("activetab");
+      $(".tab-content").removeClass("activetab-content");
+    }
+    $("#tabs span").each(function (i) {
+      $(this).on("click", function () {
+        removeActive();
+        $(this).addClass("activetab");
+        $(".tab-content").eq(i).addClass("activetab-content");
+        qlik.resize();
+      });
+    });
+  });
+});
+
+//callbacks -- inserted here --
+function CustomTable(reply, app) {}
+
+//create cubes and lists -- inserted here --
+app.createCube(
+  {
+    qInitialDataFetch: [
+      {
+        qHeight: 20,
+        qWidth: 2,
+      },
+    ],
+    qDimensions: [
+      {
+        qDef: {
+          qFieldDefs: ["Product Category"],
+        },
+        qNullSuppression: true,
+        qOtherTotalSpec: {
+          qOtherMode: "OTHER_OFF",
+          qSuppressOther: true,
+          qOtherSortMode: "OTHER_SORT_DESCENDING",
+          qOtherCounted: {
+            qv: "5",
+          },
+          qOtherLimitMode: "OTHER_GE_LIMIT",
+        },
+      },
+    ],
+    qMeasures: [
+      {
+        qDef: {
+          qDef: "Count(Product)",
+        },
+        qLabel: "Count(Product)",
+        qLibraryId: null,
+        qSortBy: {
+          qSortByState: 0,
+          qSortByFrequency: 0,
+          qSortByNumeric: 0,
+          qSortByAscii: 1,
+          qSortByLoadOrder: 0,
+          qSortByExpression: 0,
+          qExpression: {
+            qv: " ",
+          },
+        },
+      },
+    ],
+    qSuppressZero: false,
+    qSuppressMissing: false,
+    qMode: "S",
+    qInterColumnSortOrder: [],
+    qStateName: "$",
+  },
+  CustomTable
+);
